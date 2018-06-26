2b6bc4d5d15d59c4255744e9d77894938e53daff
olaven <olavsundfoer@gmail.com>
2018-06-26 18:38:20 +0200

    improved image responsiveness

--- src/Components/ImageView/ImageView.tsx
+++ src/Components/ImageView/ImageView.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+
 import "./ImageView.css";
+
 
 // Components 
 import ImageBlock from '../ImageBlock/ImageBlock'
@@ -12,35 +14,19 @@ interface IImageViewProps {
 class ImageView extends React.Component<IImageViewProps> {
     
     public render() {
-        return <div className="ImageView-column">
-            {/*
-                div column 
-                    div row 
-                        x img 
-                    div row 
-                        x img 
-            */}
-            <div className=".ImageView-row">
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            </div>
-            <div className=".ImageView-row ">
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            </div>
-            <div className=".ImageView-row ">
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" /> <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-                <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            </div>
+        return <div className="ImageView">
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
         </div>;
     }
 }
