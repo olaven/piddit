281b42f7e3afcaeac3e558ba6bf8aa2b87401dfc
olaven <olavsundfoer@gmail.com>
2018-06-26 20:31:22 +0200

    images are fetched and displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
 // Components 
 import ImageView from './Components/ImageView/ImageView'; 
@@ -9,7 +9,8 @@ interface IAppState {
   options: Array<{
     text: string,
     action: () => void
-  }>
+  }>; 
+  images : [string, string][]
 }
 
 class App extends React.Component<{}, IAppState> {
@@ -28,15 +29,56 @@ class App extends React.Component<{}, IAppState> {
           action: () => {alert("option 2 was clicked")}, 
           text: "option 2"
         }
-      ]
-    })
+      ], 
+      images : []
+    }); 
+  }
+
+  public componentDidMount() {
+    this.getImages("infrastructureporn", results => {
+      this.setState({
+        images : results
+      })
+    }); 
   }
 
   public render() {
     return <div>
         <Sidebar header="Options" options={this.state.options} />
-        <ImageView images={[["hei", "hei igjen"]]} />
+        <ImageView images={this.state.images} />
       </div>;
+  }
+
+  /**
+   * @param subreddit name of the subreddit to search. 
+   * @param callback the callback for results. 
+   */
+  private getImages(subreddit : string, callback : (results : [string, string][]) => any) : any {
+    
+    let sources : [string, string][] = []; 
+
+    fetch("https://www.reddit.com/r/" + subreddit + "/.json")
+      .then(response => response.json().then(listing => {
+        sources = this.getSources(listing.data.children); 
+        callback(sources); 
+      }))
+      .catch(err => console.error(err));
+  }
+
+  /**
+   * Turn the data-object from Reddit API into list of image sources 
+   * Currently works for children nodes from {kind : "listing"} repsonse 
+   * @param {object} children children nodes to get URLs from. 
+   */
+  private getSources(children : Array<any>) : [string, string][] {
+    let sources : [string, string][] = []; 
+    for(let child of children) {
+      // only posts that are images and where url exists
+      if(child.data.post_hint === "image" && typeof child.data.url === "string") {
+        sources.push([child.data.url, child.data.title]); 
+      }
+    }
+    return sources; 
   }
 }
 
--- src/Components/ImageView/ImageView.tsx
+++ src/Components/ImageView/ImageView.tsx
@@ -1,33 +1,30 @@
 import * as React from "react";
-
+// Components 
+import ImageBlock from '../ImageBlock/ImageBlock';
 import "./ImageView.css";
 
 
-// Components 
-import ImageBlock from '../ImageBlock/ImageBlock'
+
 
 interface IImageViewProps {
             // source, alt 
-    images : [[string, string]]
+    images : [string, string][]
 }
 
-class ImageView extends React.Component<IImageViewProps> {
-    
+class ImageView extends React.Component<IImageViewProps> {    
     public render() {
         return <div className="ImageView">
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
-            <ImageBlock source="https://i.imgur.com/dnSlUkv.jpg" alt="Image of train" />
+            {this.renderImages()}
         </div>;
+    }
+
+    private renderImages() {
+        return this.props.images.map(image => {
+            return <div>
+                <h1>Bilde</h1>
+                <ImageBlock source={image[0]} alt={image[1]} key={image[0]} />
+            </div>;
+        })
     }
 }
 
