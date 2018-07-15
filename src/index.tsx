5bbd2a082ac84ca982883fd907725297257eefd6
olaven <olavsundfoer@gmail.com>
2018-07-15 11:58:52 +0200

    improvements to become PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+// Components
 import App from './App';
+// Stylesheets
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// Redirecting all traffic to HTTPS 
+const host = "sundfoer.consulting";
+if (window.location.host == host && window.location.protocol != "https:") {
+  window.location.protocol = "https:";
+}
 
 ReactDOM.render(
-  <App />,
+  <App />, 
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
